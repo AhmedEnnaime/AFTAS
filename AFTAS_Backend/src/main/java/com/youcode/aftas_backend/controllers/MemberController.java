85c6b7b183d8f133093c44f2ec30f6fba16eead4
Essoulihamza <ehamza.web@gmail.com>
2023-12-09 13:36:42 +0100

    haak a said tbara3333

--- AFTAS_Backend/src/main/java/com/youcode/aftas_backend/controllers/MemberController.java
+++ AFTAS_Backend/src/main/java/com/youcode/aftas_backend/controllers/MemberController.java
@@ -4,6 +4,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.youcode.aftas_backend.models.dto.Member.MemberDto;
+import com.youcode.aftas_backend.superClasses.Controller;
+
 @RestController
 @RequestMapping(path = "api/members", produces = MediaType.APPLICATION_JSON_VALUE)
-public class MemberController {}
+public class MemberController extends Controller<MemberDto, Integer> {}
--- AFTAS_Backend/src/main/java/com/youcode/aftas_backend/controllers/RankingController.java
+++ AFTAS_Backend/src/main/java/com/youcode/aftas_backend/controllers/RankingController.java
@@ -4,6 +4,10 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.youcode.aftas_backend.models.dto.RankingDto;
+import com.youcode.aftas_backend.models.embeddables.CompetitionMember;
+import com.youcode.aftas_backend.superClasses.Controller;
+
 @RestController
 @RequestMapping(path = "api/rankings", produces = MediaType.APPLICATION_JSON_VALUE)
-public class RankingController {}
+public class RankingController extends Controller<RankingDto, CompetitionMember> {}
