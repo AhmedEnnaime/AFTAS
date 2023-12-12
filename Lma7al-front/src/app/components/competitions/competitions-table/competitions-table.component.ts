c8fc678514e608c949a4e5c2f61e5aff237d83da
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-12-12 22:17:04 +0100

    ba9i mdarbin m3a had zmar

--- Lma7al-front/src/app/components/competitions/competitions-table/competitions-table.component.ts
+++ Lma7al-front/src/app/components/competitions/competitions-table/competitions-table.component.ts
@@ -2,6 +2,8 @@ import {Component, Input} from '@angular/core';
 import {Observable} from "rxjs";
 import {Competition} from "../../../model/interfaces/competition.model";
 import {Router} from "@angular/router";
+import * as competitionPageActions from "../../../store/competition/actions/competition-page.actions";
+import {Store} from "@ngrx/store";
 
 @Component({
   selector: 'app-competitions-table',
@@ -10,9 +12,22 @@ import {Router} from "@angular/router";
 })
 export class CompetitionsTableComponent {
   @Input() competitions?: Observable<Competition[]>
+  showDeleteModal: boolean = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private store: Store) {
   }
+
+  displayDeleteModal() {
+    console.log("clicked")
+    this.showDeleteModal = true;
+    console.log(this.showDeleteModal)
+  }
+
+  // deleteCompetition(code: String | undefined) {
+  //   this.store.dispatch(
+  //     competitionPageActions.deleteCompetition({ CompetitionCode: code })
+  //   );
+  // }
 
   navigateToRankings(id: String) {
     this.router.navigate(['/rankings', id]);
