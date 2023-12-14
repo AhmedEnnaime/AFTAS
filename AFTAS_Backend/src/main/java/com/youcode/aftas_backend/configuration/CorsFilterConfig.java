806ca63ab0e5af956607c85f56a1212827603b52
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-12-14 17:11:12 +0100

    tzala9

--- AFTAS_Backend/src/main/java/com/youcode/aftas_backend/configuration/CorsFilterConfig.java
+++ AFTAS_Backend/src/main/java/com/youcode/aftas_backend/configuration/CorsFilterConfig.java
@@ -16,7 +16,7 @@ public class CorsFilterConfig {
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4201", "http://localhost:5678/", "http://localhost:1234"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4201", "http://localhost:80", "http://localhost:8080", "http://localhost", "http://localhost:5678/", "http://localhost:1234"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
