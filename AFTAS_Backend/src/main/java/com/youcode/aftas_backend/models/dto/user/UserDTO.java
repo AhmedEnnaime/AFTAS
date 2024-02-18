907ef1ae4847df8877101cecd6ab9ba840dd6c8d
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2024-02-18 16:30:46 +0100

    added user dto and role dto

---
+++ AFTAS_Backend/src/main/java/com/youcode/aftas_backend/models/dto/user/UserDTO.java
@@ -0,0 +1,21 @@
+package com.youcode.aftas_backend.models.dto.user;
+
+import java.util.Set;
+
+import com.youcode.aftas_backend.models.dto.role.RoleDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String password;
+    private Set<RoleDTO> roles;
+}
