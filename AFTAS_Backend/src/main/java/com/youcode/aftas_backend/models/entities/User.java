b64b6928360f919c62a4728daad5d650c3699b24
Essoulihamza <ehamza.web@gmail.com>
2024-02-20 14:22:56 +0100

    ee

--- AFTAS_Backend/src/main/java/com/youcode/aftas_backend/models/entities/User.java
+++ AFTAS_Backend/src/main/java/com/youcode/aftas_backend/models/entities/User.java
@@ -19,7 +19,6 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
